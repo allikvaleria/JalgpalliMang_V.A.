541aa9121a8f9dbfa382ddc9f49308c89970c1af
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2024-10-24 20:09:30 +0300

    Update Program.cs

--- JalgpalliMang_V.A.TARpv23/Program.cs
+++ JalgpalliMang_V.A.TARpv23/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace JalgpalliMang_V.A.TARpv23
@@ -8,11 +10,38 @@ namespace JalgpalliMang_V.A.TARpv23
         static void Main(string[] args)
         {
             // Создание стадиона
-            Stadium stadium = new Stadium(80, 20);
+            var width = 80;
+            var height = 20;
 
-            // Создание игры
-            Game game = new Game(stadium);
-            game.Start(); // Запуск игры
+            var homeTeam = new Team("HomeTeam");
+            var awayTeam = new Team("AwayTeam");
+
+            var stadium = new Stadium(width, height);
+
+            // Добавляем 11 игроков в каждую команду
+            for (int i = 1; i <= 11; i++)
+            {
+                homeTeam.AddPlayer(new Player($"HomeTeamPlayer {i}", stadium, null));
+                awayTeam.AddPlayer(new Player($"AwayTeamPlayer {i}", stadium, null));
+            }
+
+            var game = new Game(homeTeam, awayTeam, stadium);
+            homeTeam.AssignGame(game); // Устанавливаем игру для команды
+            awayTeam.AssignGame(game); // Устанавливаем игру для команды
+            game.Start();
+
+            // Основной игровой цикл
+            while (true)
+            {
+                stadium.Draw();
+                homeTeam.Draw();
+                awayTeam.Draw();
+                game.Ball.Draw();
+                homeTeam.Move();
+                awayTeam.Move();
+                game.Move();
+                Thread.Sleep(300);
+            }
         }
     }
-}
+ }
